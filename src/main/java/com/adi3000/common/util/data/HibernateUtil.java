59e136d3a781e4244d89498e55473960bc055fb4
adi3000 <adi3000@free.fr>
2013-11-05 18:11:06 +0100

    Issue with initializing a null proxy

--- src/main/java/com/adi3000/common/util/data/HibernateUtil.java
+++ src/main/java/com/adi3000/common/util/data/HibernateUtil.java
@@ -9,11 +9,15 @@ import com.adi3000.common.database.hibernate.data.DataObject;
 public final class HibernateUtil {
 
 	public static void initialize(Collection<? extends DataObject> list){
-		for(DataObject o : list){
-			Hibernate.initialize(o);
+		if(list != null){
+			for(DataObject o : list){
+				Hibernate.initialize(o);
+			}
 		}
 	}
 	public static void initialize(DataObject proxy){
+		if(proxy != null){
 			Hibernate.initialize(proxy);
+		}
 	}
 }
