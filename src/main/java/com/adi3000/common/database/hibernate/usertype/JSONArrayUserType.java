914ffe0f72fb4bb83e8f4c4aec2d993b88c9571d
adi <adi3000@free.fr>
2014-01-26 19:57:21 +0100

    Issue when retrive data

--- src/main/java/com/adi3000/common/database/hibernate/usertype/JSONArrayUserType.java
+++ src/main/java/com/adi3000/common/database/hibernate/usertype/JSONArrayUserType.java
@@ -12,7 +12,7 @@ import org.codehaus.jettison.json.JSONArray;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.usertype.UserType;
- 
+
 /**
  * Hibernate user type to persist JSONArray
  *
@@ -21,81 +21,82 @@ import org.hibernate.usertype.UserType;
  * @author "Jan Jonas <mail@janjonas.net>"
  */
 public class JSONArrayUserType implements UserType {
- 
-  private static final int[] SQL_TYPES = { Types.VARCHAR};
- 
-  @Override
-  public Object assemble(Serializable cached, Object owner) throws HibernateException {
-    return deepCopy(cached);
-  }
- 
-  @Override
-  public Object deepCopy(Object value) throws HibernateException {
-    if (value == null) return value;
-    try {
-      return new JSONArray(((JSONArray)value).toString());
-    } catch (JSONException e) {
-      throw new RuntimeException(e);
-    }
-  }
- 
-  @Override
-  public Serializable disassemble(Object value) throws HibernateException {
-    return ((JSONArray)value).toString();
-  }
- 
-  @Override
-  public boolean equals(Object x, Object y) throws HibernateException {
-    if (x == null) return (y != null);
-    return (x.equals(y));
-  }
- 
-  @Override
-  public int hashCode(Object x) throws HibernateException {
-    return ((JSONArray)x).toString().hashCode();
-  }
- 
-  @Override
-  public boolean isMutable() {
-    return true;
-  }
- 
-  @Override
-  public Object nullSafeGet(ResultSet rs, String[] names,
+
+	private static final int[] SQL_TYPES = { Types.VARCHAR};
+
+	@Override
+	public Object assemble(Serializable cached, Object owner) throws HibernateException {
+		return deepCopy(cached);
+	}
+
+	@Override
+	public Object deepCopy(Object value) throws HibernateException {
+		if (value == null) return value;
+		try {
+			return new JSONArray(((JSONArray)value).toString());
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public Serializable disassemble(Object value) throws HibernateException {
+		return ((JSONArray)value).toString();
+	}
+
+	@Override
+	public boolean equals(Object x, Object y) throws HibernateException {
+		if (x == null) return (y != null);
+		return (x.equals(y));
+	}
+
+	@Override
+	public int hashCode(Object x) throws HibernateException {
+		return ((JSONArray)x).toString().hashCode();
+	}
+
+	@Override
+	public boolean isMutable() {
+		return true;
+	}
+
+	@Override
+	public Object nullSafeGet(ResultSet rs, String[] names,
 			SessionImplementor session, Object owner) throws HibernateException, SQLException {
-    if (!rs.wasNull()) {
-      try {
-        return new JSONArray(rs.getString(names[0]));
-      } catch (JSONException e) {
-        throw new RuntimeException(e);
-      }
-    }
-    return null;
-  }
- 
-  @Override
-  public void nullSafeSet(PreparedStatement st, Object value, int index,
+		String value = rs.getString(names[0]);
+		if (!rs.wasNull()) {
+			try {
+				return new JSONArray(value);
+			} catch (JSONException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void nullSafeSet(PreparedStatement st, Object value, int index,
 			SessionImplementor session) throws HibernateException, SQLException {
-    if (value == null) {
-      st.setNull(index, SQL_TYPES[0]);
-    } else {
-      st.setString(index, ((JSONArray)value).toString());
-    }
-  }
- 
-  @Override
-  public Object replace(Object original, Object target, Object owner) throws HibernateException {
-    return deepCopy(original);
-  }
- 
-  @Override
-  public Class<JSONArray> returnedClass() {
-    return JSONArray.class;
-  }
- 
-  @Override
-  public int[] sqlTypes() {
-    return SQL_TYPES;
-  }
- 
+		if (value == null) {
+			st.setNull(index, SQL_TYPES[0]);
+		} else {
+			st.setString(index, ((JSONArray)value).toString());
+		}
+	}
+
+	@Override
+	public Object replace(Object original, Object target, Object owner) throws HibernateException {
+		return deepCopy(original);
+	}
+
+	@Override
+	public Class<JSONArray> returnedClass() {
+		return JSONArray.class;
+	}
+
+	@Override
+	public int[] sqlTypes() {
+		return SQL_TYPES;
+	}
+
 }
--- src/main/java/com/adi3000/common/database/hibernate/usertype/JSONObjectUserType.java
+++ src/main/java/com/adi3000/common/database/hibernate/usertype/JSONObjectUserType.java
@@ -12,7 +12,7 @@ import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.usertype.UserType;
- 
+
 /**
  * Hibernate user type to persist JSONObject
  *
@@ -21,81 +21,82 @@ import org.hibernate.usertype.UserType;
  * @author "Jan Jonas <mail@janjonas.net>"
  */
 public class JSONObjectUserType implements UserType {
- 
-  private static final int[] SQL_TYPES = { Types.VARCHAR};
- 
-  @Override
-  public Object assemble(Serializable cached, Object owner) throws HibernateException {
-    return deepCopy(cached);
-  }
- 
-  @Override
-  public Object deepCopy(Object value) throws HibernateException {
-    if (value == null) return value;
-    try {
-      return new JSONObject(((JSONObject)value).toString());
-    } catch (JSONException e) {
-      throw new RuntimeException(e);
-    }
-  }
- 
-  @Override
-  public Serializable disassemble(Object value) throws HibernateException {
-    return ((JSONObject)value).toString();
-  }
- 
-  @Override
-  public boolean equals(Object x, Object y) throws HibernateException {
-    if (x == null) return (y != null);
-    return (x.equals(y));
-  }
- 
-  @Override
-  public int hashCode(Object x) throws HibernateException {
-    return ((JSONObject)x).toString().hashCode();
-  }
- 
-  @Override
-  public boolean isMutable() {
-    return true;
-  }
- 
-  @Override
-  public Object nullSafeGet(ResultSet rs, String[] names,
+
+	private static final int[] SQL_TYPES = { Types.VARCHAR};
+
+	@Override
+	public Object assemble(Serializable cached, Object owner) throws HibernateException {
+		return deepCopy(cached);
+	}
+
+	@Override
+	public Object deepCopy(Object value) throws HibernateException {
+		if (value == null) return value;
+		try {
+			return new JSONObject(((JSONObject)value).toString());
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public Serializable disassemble(Object value) throws HibernateException {
+		return ((JSONObject)value).toString();
+	}
+
+	@Override
+	public boolean equals(Object x, Object y) throws HibernateException {
+		if (x == null) return (y != null);
+		return (x.equals(y));
+	}
+
+	@Override
+	public int hashCode(Object x) throws HibernateException {
+		return ((JSONObject)x).toString().hashCode();
+	}
+
+	@Override
+	public boolean isMutable() {
+		return true;
+	}
+
+	@Override
+	public Object nullSafeGet(ResultSet rs, String[] names,
 			SessionImplementor session, Object owner) throws HibernateException, SQLException {
-    if (!rs.wasNull()) {
-      try {
-        return new JSONObject(rs.getString(names[0]));
-      } catch (JSONException e) {
-        throw new RuntimeException(e);
-      }
-    }
-    return null;
-  }
- 
-  @Override
-  public void nullSafeSet(PreparedStatement st, Object value, int index,
+		String value = rs.getString(names[0]);
+		if (!rs.wasNull()) {
+			try {
+				return new JSONObject(value);
+			} catch (JSONException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void nullSafeSet(PreparedStatement st, Object value, int index,
 			SessionImplementor session) throws HibernateException, SQLException {
-    if (value == null) {
-      st.setNull(index, SQL_TYPES[0]);
-    } else {
-      st.setString(index, ((JSONObject)value).toString());
-    }
-  }
- 
-  @Override
-  public Object replace(Object original, Object target, Object owner) throws HibernateException {
-    return deepCopy(original);
-  }
- 
-  @Override
-  public Class<JSONObject> returnedClass() {
-    return JSONObject.class;
-  }
- 
-  @Override
-  public int[] sqlTypes() {
-    return SQL_TYPES;
-  }
- 
+		if (value == null) {
+			st.setNull(index, SQL_TYPES[0]);
+		} else {
+			st.setString(index, ((JSONObject)value).toString());
+		}
+	}
+
+	@Override
+	public Object replace(Object original, Object target, Object owner) throws HibernateException {
+		return deepCopy(original);
+	}
+
+	@Override
+	public Class<JSONObject> returnedClass() {
+		return JSONObject.class;
+	}
+
+	@Override
+	public int[] sqlTypes() {
+		return SQL_TYPES;
+	}
+
 }
